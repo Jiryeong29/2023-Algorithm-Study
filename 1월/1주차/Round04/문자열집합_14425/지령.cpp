fe64283e443a8ada9b04530c6510043ae36ae652
Jiryeong29 <52111476+Jiryeong29@users.noreply.github.com>
2023-01-19 13:50:32 +0900

    UPDATE지령.cpp

--- 1월/1주차/Round04/문자열집합_14425/지령.cpp
+++ 1월/1주차/Round04/문자열집합_14425/지령.cpp
@@ -1,1 +1,25 @@
-// 푸는중..
+// 문자열집합 14425
+#include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int main() {
+	int n, m,cnt=0;
+	cin >> n >> m;
+	string str;
+	set<string> s1;	
+
+	for (int i = 0; i < n; i++) {
+		cin >> str;
+		s1.insert(str);
+	}
+	for (int i = 0; i < m; i++) {
+		cin >> str;
+		if (s1.find(str) != s1.end()) {
+			cnt++;
+		}
+	}
+	cout << cnt;
+}
