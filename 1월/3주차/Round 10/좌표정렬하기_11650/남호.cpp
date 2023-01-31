14663cd9f30a79d88d2fdb58e0ac226492247424
1829122 <100738276+1829122@users.noreply.github.com>
2023-01-31 22:33:18 +0900

    Create 남호.cpp

---
+++ 1월/3주차/Round 10/좌표정렬하기_11650/남호.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <iostream>
+#include <string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+	int n, x, y;
+	vector<pair<long long, long long>> v;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> x >> y;
+		v.push_back({ x, y });
+	}
+	sort(v.begin(), v.end());
+
+	for (auto a : v) cout << a.first << " " << a.second << '\n';
+
+	
+}
