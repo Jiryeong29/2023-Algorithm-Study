358cc37c5fcb42f87227014bfc2c01a8499c64ee
Jiryeong29 <52111476+Jiryeong29@users.noreply.github.com>
2023-02-01 11:49:12 +0900

    Update 지령.cpp

--- 1월/3주차/Round 11/나이순정렬_10814/지령.cpp
+++ 1월/3주차/Round 11/나이순정렬_10814/지령.cpp
@@ -1,1 +1,35 @@
 
+#include<stdio.h>
+#include <iostream>
+#include <string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<cmath>
+#define endl '\n'
+using namespace std;
+bool compare(pair<int, string> a, pair<int, string> b)
+{
+	return a.first < b.first;
+}
+
+int main()
+{
+	int n, x;
+	string y;
+	cin >> n;
+
+	vector<pair<int, string>> v;
+
+	for (int i = 0; i < n; ++i) {
+		cin >> x >> y;
+		v.push_back({ x, y });
+	}
+
+	stable_sort(v.begin(), v.end(),compare);
+
+	for (int i = 0; i < n; ++i) {
+		cout << v[i].first << " " << v[i].second << endl;
+	}
+}
