c45956448c706b951933ce46271bff23b62088a3
1829122 <100738276+1829122@users.noreply.github.com>
2023-02-12 16:00:38 +0900

    Create 남호.cpp

---
+++ 1월/4주차/Round 12/재귀함수가뭔가요_17478/남호.cpp
@@ -0,0 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include <iostream>
+#include <string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<cmath>
+using namespace std;
+
+int n;
+string str = "";
+
+void fibo(int cnt) {
+
+	string aa = str;
+
+	if (cnt == n) {
+		cout << aa << "\"재귀함수가 뭔가요?\"\n";
+		cout << aa << "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
+		cout << aa << "라고 답변하였지.\n";
+		return;
+	}
+
+	cout << aa << "\"재귀함수가 뭔가요?\"\n";
+	cout << aa << "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n";
+	cout << aa << "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n";
+	cout << aa << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
+
+	str += "____";
+	fibo(cnt + 1);
+
+	cout << aa << "라고 답변하였지.\n";
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	
+	cin >> n;
+
+	cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n";
+	fibo(0);
+}
